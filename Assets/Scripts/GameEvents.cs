36c2288b88cfbf9538a43777c972e90c077ce7b3
hoadq <hoadinhquoc@gmail.com>
2018-11-10 20:16:28 +0700

    +basic menu

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GameEvents {
+
+	public static Action GAME_START;
+}
+
+public static class ActionExt
+{
+	public static void Raise(this Action action)
+	{
+		if (action != null)
+			action.Invoke();
+	}
+	public static void Raise<T1>(this Action<T1> action, T1 value1)
+	{
+		if (action != null)
+			action.Invoke(value1);
+	}
+
+	public static void Raise<T1, T2>(this Action<T1, T2> action, T1 value1, T2 value2)
+	{
+		if (action != null)
+			action.Invoke(value1, value2);
+	}
+
+	public static void Raise<T1, T2, T3>(this Action<T1, T2, T3> action, T1 value1, T2 value2, T3 value3)
+	{
+		if (action != null)
+			action.Invoke(value1, value2, value3);
+	}
+}
